7f8f359995b23afe859c2938a64443aa2a33c8d1
tmkhitaryan899-prog <tmkhitaryan899@gmail.com>
2026-02-07 16:02:46 +0400

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import {ThemeProvider,} from "./Utils/ThemeContext.jsx";
 import {Provider} from "react-redux";
-import {store} from "./Redux/Store";
+import {store} from "./Redux/store";
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
